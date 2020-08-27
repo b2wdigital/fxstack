8a7384a20960cb713add17a3063a75eeda52402a
João Paulo Faria <jpfaria@gmail.com>
2020-08-27 09:59:47 -0300

    fixing sqs

--- serverless/lambda/sqs.go
+++ serverless/lambda/sqs.go
@@ -37,36 +37,32 @@ func fromSQS(parentCtx context.Context, event Event) []*cloudevents.InOut {
 
 			in := v2.NewEvent()
 
-			if err = json.Unmarshal([]byte(record.Body), &in); err != nil {
-				err = errors.NewNotValid(err, "could not decode SQS record")
-			} else {
+			if er := json.Unmarshal([]byte(record.Body), &in); er != nil {
 
 				var data interface{}
 
 				snsEvent := events.SNSEntity{}
 
-				if err = json.Unmarshal([]byte(record.Body), &snsEvent); err != nil {
-					err = errors.NewNotValid(err, "could not decode SQS record")
-				} else {
+				if err = json.Unmarshal([]byte(record.Body), &snsEvent); err == nil {
 
 					if snsEvent.Message != "" {
 
-						if err = json.Unmarshal([]byte(snsEvent.Message), &data); err != nil {
-							err = errors.NewNotValid(err, "could not decode SNS message from SQS record")
+						if er := json.Unmarshal([]byte(snsEvent.Message), &data); er != nil {
+							err = errors.NewNotValid(er, "could not decode SNS message from SQS record")
 						}
 
 					} else {
 
-						if err = json.Unmarshal([]byte(record.Body), &data); err != nil {
-							err = errors.NewNotValid(err, "could not decode SQS record")
+						if er := json.Unmarshal([]byte(record.Body), &data); er != nil {
+							err = errors.NewNotValid(er, "could not decode SQS record")
 						}
 
 					}
 
-				}
+					if er := in.SetData("", data); er != nil {
+						err = errors.NewNotValid(er, "could not set data in event")
+					}
 
-				if err = in.SetData("", data); err != nil {
-					err = errors.NewNotValid(err, "could not set data in event")
 				}
 
 			}
